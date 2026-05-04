8e82ea2db8469db85f63a1593380eb86c90f4302
quan-ng03 <jacob.nguyenaq@gmail.com>
2026-05-04 10:48:26 -0700

    Deploy site to Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
 import path from "node:path";
 import type { NextConfig } from "next";
 
+const repoName = "pho99andgrill";
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isGithubPages ? `/${repoName}` : "",
+  assetPrefix: isGithubPages ? `/${repoName}/` : "",
+
   turbopack: {
     root: path.join(__dirname),
   },
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
